91dcb8062bc1827227c2b74621e0fd16ceac76f5
Emmanuel Larbi <117349144+manuel-larbi@users.noreply.github.com>
2024-06-27 16:17:23 +0000

    added a dedicated exception handler

---
+++ src/main/java/com/manuel/sso_security_context/exception/CustomContextException.java
@@ -0,0 +1,7 @@
+package com.manuel.sso_security_context.exception;
+
+public class CustomContextException extends RuntimeException {
+    public CustomContextException(Throwable message) {
+        super(message);
+    }
+}
